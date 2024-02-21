fa5354767a9b3c936f33ff6e967ad0fabb72ed36
davidkim205 <davidkim205@gmail.com>
2024-02-21 18:30:08 +0900

    nllb200 cuda 실행되도록 코드 수정

--- models/nllb200.py
+++ models/nllb200.py
@@ -2,8 +2,7 @@ from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 from utils.bleu_score import simple_score
 
 model = AutoModelForSeq2SeqLM.from_pretrained("facebook/nllb-200-distilled-1.3B", device_map="auto")
-tokenizer = AutoTokenizer.from_pretrained("facebook/nllb-200-distilled-1.3B", device_map="auto")
-
+tokenizer = AutoTokenizer.from_pretrained("facebook/nllb-200-distilled-1.3B")
 
 
 def translate_ko2en(text):
@@ -11,7 +10,7 @@ def translate_ko2en(text):
     inputs = tokenizer(batched_input, return_tensors="pt", padding=True)
 
     translated_tokens = model.generate(
-        **inputs, forced_bos_token_id=tokenizer.lang_code_to_id["eng_Latn"]
+        **inputs.to(model.device), forced_bos_token_id=tokenizer.lang_code_to_id["eng_Latn"]
     )
     result = tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
     return result
@@ -22,7 +21,7 @@ def translate_en2ko(text):
     inputs = tokenizer(batched_input, return_tensors="pt", padding=True)
 
     translated_tokens = model.generate(
-        **inputs, forced_bos_token_id=tokenizer.lang_code_to_id["kor_Hang"], max_new_tokens=2048)
+        **inputs.to(model.device), forced_bos_token_id=tokenizer.lang_code_to_id["kor_Hang"], max_new_tokens=2048)
 
     result = tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
     return result
@@ -42,6 +41,7 @@ en_text There are over 30 million files and 250 billion tokens. Phi1.5's data se
 ko_text 300만 개 이상의 파일과 25억 개의 토큰이 있습니다. Phi1.5의 데이터 세트 구성은 액세스 가능하지만 오픈 소스 모델 Mixtral 8x7B를 사용하고 Apache 2.0 라이선스에 따라 라이선스됩니다.
 score 0.3090015909429233
     """
+
 
 if __name__ == "__main__":
     main()
