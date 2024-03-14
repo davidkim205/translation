b04c5f1f3a6f1cf9b2859747357004f49651feca
sudog1 <eric2digit@gmail.com>
2024-03-14 17:36:19 +0900

    번역 코드 개선 및 데이터 추가(#5)

--- translation.py
+++ translation.py
@@ -6,44 +6,6 @@ from utils.bleu_score import simple_score
 import re
 
 
-# 결과 파일 경로이름을 생성
-def gen_output_filename(filename, model):
-    name, extension = os.path.splitext(os.path.basename(filename))
-    return f"llm_ko_datasets/ko_{name}_{model}{extension}"
-
-
-def load_json(filename, n):
-    json_data = []
-    with open(filename, "r", encoding="utf-8") as f:
-        if os.path.splitext(filename)[1] != ".jsonl":
-            json_data = json.load(f)
-        else:
-            for i, line in enumerate(f):
-                # if i < n:
-                #     continue
-                # elif i >= n + 200:
-                #     break
-                json_data.append(json.loads(line))
-    return json_data
-
-
-def get_line_count(filename):
-    with open(filename, "r", encoding="utf-8") as f:
-        return sum(1 for _ in f)
-
-
-# 파일이 존재하면 마지막 줄 뒤에 추가합니다.
-def save_json(json_data, filename, option="a"):
-    filename = filename.replace(" ", "_")
-    with open(filename, option, encoding="utf-8") as f:
-        if not filename.endswith(".jsonl"):
-            json.dump(json_data, f, ensure_ascii=False, indent=4)
-        else:
-            for data in json_data:
-                json.dump(data, f, ensure_ascii=False)
-                f.write("\n")
-
-
 # 대화 번역 트랜잭션
 # def cloud_translation(translate_en2ko, api, conversations):
 #     for conversation in conversations:
@@ -126,58 +88,77 @@ def translate_ko_hellaswag(translate_en2ko, api, row):
     return True
 
 
+def gen_output_filename(filename, model):
+    name, extension = os.path.splitext(os.path.basename(filename))
+    return f"llm_ko_datasets/ko_{name}_{model}{extension}"
+
+
+def load_json(filename, n, a):
+    json_data = []
+    with open(filename, "r", encoding="utf-8") as f:
+        if os.path.splitext(filename)[1] != ".jsonl":
+            json_data = json.load(f)
+        else:
+            for i, line in enumerate(f):
+                if i < n:
+                    continue
+                elif i >= n + a:
+                    break
+                json_data.append(json.loads(line))
+    return json_data
+
+
+def get_line_count(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        return sum(1 for _ in f)
+
+
+# 파일이 존재하면 마지막 줄 뒤에 추가합니다.
+def save_json(json_data, filename, option="a"):
+    filename = filename.replace(" ", "_")
+    with open(filename, option, encoding="utf-8") as f:
+        if not filename.endswith(".jsonl"):
+            json.dump(json_data, f, ensure_ascii=False, indent=4)
+        else:
+            for data in json_data:
+                json.dump(data, f, ensure_ascii=False)
+                f.write("\n")
+
+
 def local_translate(translate_en2ko, translate_ko2en, row, attrs):
     korean_pattern = re.compile("[가-힣]")
     for attr in attrs:
         text = row[attr]
         del row[attr]
         row[attr] = text
+        row[f"ko_{attr}"] = ""
+        row[f"en_{attr}"] = ""
+        row[f"bleu_{attr}"] = -1
         try:
-            if not f"ko_{attr}" in row:
-                if row[attr]:
-                    row[f"ko_{attr}"] = translate_en2ko(row[attr])
-                else:
-                    row[f"ko_{attr}"] = ""
-            else:
-                text = row[f"ko_{attr}"]
-                del row[f"ko_{attr}"]
-                row[f"ko_{attr}"] = text
-            # if not korean_pattern.search(row[f"ko_{attr}"]):
-            #     if f"en_{attr}" in row:
-            #         del row[f"en_{attr}"]
-            if not f"en_{attr}" in row:
-                if korean_pattern.search(row[f"ko_{attr}"]):
-                    row[f"en_{attr}"] = translate_ko2en(row[f"ko_{attr}"])
-                else:
-                    row[f"en_{attr}"] = ""
-            else:
-                text = row[f"en_{attr}"]
-                del row[f"en_{attr}"]
-                row[f"en_{attr}"] = text
-            if not f"bleu_{attr}" in row:
-                row[f"bleu_{attr}"] = simple_score(row[attr], row[f"en_{attr}"])
-            else:
-                text = row[f"bleu_{attr}"]
-                del row[f"bleu_{attr}"]
-                row[f"bleu_{attr}"] = text
-            # row[f"bleu_{attr}"] = simple_score(row[attr], row[f"en_{attr}"])
+            if not row[attr]:
+                continue
+            row[f"ko_{attr}"] = translate_en2ko(row[attr])
+            if not korean_pattern.search(row[f"ko_{attr}"]):
+                continue
+            row[f"en_{attr}"] = translate_ko2en(row[f"ko_{attr}"])
+            if not row[f"en_{attr}"]:
+                continue
+            row[f"bleu_{attr}"] = simple_score(row[attr], row[f"en_{attr}"])
         except Exception as e:
             print(e)
-    return True
 
 
 def main():
     parser = argparse.ArgumentParser("argument")
-    parser.add_argument(
-        "--input_file",
-        default="./llm_datasets/conversation_arc.jsonl",
-        type=str,
-        help="input_file",
-    )
-    parser.add_argument("--model", default="TowerInstruct", type=str, help="model")
-    parser.add_argument(
-        "--dataset", default="ko_arc_challenge", type=str, help="dataset"
-    )
+    # parser.add_argument(
+    #     "--input_file",
+    #     default="./llm_datasets/conversation_arc.jsonl",
+    #     type=str,
+    #     help="input_file",
+    # )
+    parser.add_argument("--model", type=str, help="model")
+    parser.add_argument("--dataset", type=str, help="dataset")
+    parser.add_argument("--lines", type=int, help="lines")
     args = parser.parse_args()
 
     if args.model == "gugugo":
@@ -190,73 +171,57 @@ def main():
         from models.nllb200 import translate_ko2en, translate_en2ko
     elif args.model == "TowerInstruct":
         from models.TowerInstruct import translate_ko2en, translate_en2ko
-    elif args.model == "cloud_api":
-        from cloud_api import translate_en2ko
+    # elif args.model == "cloud_api":
+    #     from cloud_api import translate_en2ko
 
-        # 해당 순서대로 소진 시 다음 API 호출
-        api_list = ["azure", "google"]
-        # 이미 번역된 데이터는 다시 번역하지 않게 구현
-        try:
-            result_data = load_json(gen_output_filename(args.input_file, args.model))
-        except FileNotFoundError:
-            result_data = []
-            pass
+    #     api_list = ["azure", "google"]
+    #     try:
+    #         result_data = load_json(gen_output_filename(args.input_file, args.model))
+    #     except FileNotFoundError:
+    #         result_data = []
+    #         pass
 
-        cloud_translation = {
-            "ko_arc_challenge": translate_ko_arc_challenge,
-            "ko_truthfulqa_mc1": translate_ko_truthfulaq_mc1,
-            "hellaswag": translate_ko_hellaswag,
-        }
+    #     cloud_translation = {
+    #         "ko_arc_challenge": translate_ko_arc_challenge,
+    #         "ko_truthfulqa_mc1": translate_ko_truthfulaq_mc1,
+    #         "hellaswag": translate_ko_hellaswag,
+    #     }
     dataset_attr = {
         "MetaMathQA-395K": ["original_question", "response", "query"],
         "OpenOrca": ["system_prompt", "question", "response"],
         # "python-codes-25k": ["output"],
     }
     # 번역 API 사용
-    if args.model == "cloud_api":
-        # json_data = json_data[:21]
-        json_data = json_data[len(result_data) :]
-        api_idx = 0
-        api = api_list[api_idx]
-        output_filename = gen_output_filename(args.input_file, "")
-        cloud_output_filename = gen_output_filename(args.input_file, args.model)
-        for data in tqdm(json_data):
-            try:
-                while not cloud_translation[args.dataset](translate_en2ko, api, data):
-                    api_idx += 1
-                    api = api_list[api_idx]
-                else:
-                    save_json([data], output_filename, "a")
-                    data["translation"] = f"{api}_api"
-                    save_json([data], cloud_output_filename, "a")
-            except IndexError:
-                break
-    # 로컬 모델 사용
-    else:
-        input_filename = f"/work/translation/llm_datasets/{args.dataset}.jsonl"
-        output_filename = gen_output_filename(input_filename, args.model)
-        line_count = get_line_count(output_filename)
-        json_data = load_json(output_filename, 200)
+    # if args.model == "cloud_api":
+    #     # json_data = json_data[:21]
+    #     json_data = json_data[len(result_data) :]
+    #     api_idx = 0
+    #     api = api_list[api_idx]
+    #     output_filename = gen_output_filename(args.input_file, "")
+    #     cloud_output_filename = gen_output_filename(args.input_file, args.model)
+    #     for data in tqdm(json_data):
+    #         try:
+    #             while not cloud_translation[args.dataset](translate_en2ko, api, data):
+    #                 api_idx += 1
+    #                 api = api_list[api_idx]
+    #             else:
+    #                 save_json([data], output_filename, "a")
+    #                 data["translation"] = f"{api}_api"
+    #                 save_json([data], cloud_output_filename, "a")
+    #         except IndexError:
+    #             break
+    # # 로컬 모델 사용
+    # else:
+    input_filename = f"/work/translation/llm_datasets/{args.dataset}.jsonl"
+    output_filename = gen_output_filename(input_filename, args.model)
+    line_count = get_line_count(output_filename)
+    json_data = load_json(input_filename, line_count, args.lines)
 
-        for data in tqdm(json_data):
-            local_translate(
-                translate_en2ko, translate_ko2en, data, dataset_attr[args.dataset]
-            )
-            save_json([data], output_filename)
-
-            # for conversation in data['conversations']:
-            #     text = conversation['value']
-            #     #print(text)
-            #     try:
-            #         ko_text = translate_en2ko(text)
-            #         en_text = translate_ko2en(ko_text)
-            #     except:
-            #         ko_text = ""
-            #         en_text = ""
-            #     #print('ko_text', ko_text)
-            #     conversation['ko'] = ko_text
-            #     conversation['en'] = en_text
-            # data['translation'] = args.model
+    for data in tqdm(json_data):
+        local_translate(
+            translate_en2ko, translate_ko2en, data, dataset_attr[args.dataset]
+        )
+        save_json([data], output_filename)
 
 
 if __name__ == "__main__":
