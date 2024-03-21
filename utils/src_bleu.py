ecb05500e49bfa04a31f4aec7192c7a4ab4b0824
sudog1 <eric2digit@gmail.com>
2024-03-21 18:29:29 +0900

    누락된 google 데이터 추가

--- utils/src_bleu.py
+++ utils/src_bleu.py
@@ -21,13 +21,11 @@ def main():
         "sharegpt-deepl-ko-translation": [],
         "aihub-MT-new-corpus": [],
         "aihub-socialsci": [],
-        "korean-parallel-corpora": [],
         "aihub-parallel-translation": [],
         "aihub-food": [],
         "aihub-techsci": [],
         "para_pat": [],
         "aihub-speechtype-based-machine-translation": [],
-        "koopus100": [],
         "aihub-basicsci": [],
         "aihub-broadcast-content": [],
         "aihub-patent": [],
@@ -35,15 +33,21 @@ def main():
     }
     file_list = get_file_list(args.input_file)
     for input_file in file_list:
+        arr = []
         json_data = load_json(input_file)
         for data in json_data:
+            if data["src"] not in src_list:
+                continue
             src_list[data["src"]].append(data["bleu"])
+            arr.append(data["bleu"])
         name, _ = os.path.splitext(os.path.basename(input_file))
-        # print("\n", name)
-    for key, val in src_list.items():
-        score = round(sum(val) / len(val), 2)
-        print(f"\t{key}: {score}")
-        src_list[key] = []
+        print("\n", name)
+        for key, val in src_list.items():
+            score = round(sum(val) / len(val), 2)
+            print(f"\t{key}: {score}")
+            src_list[key] = []
+        print(len(arr))
+        print("\n\tavg: ", round(sum(arr) / len(arr), 2))
 
 
 if __name__ == "__main__":
