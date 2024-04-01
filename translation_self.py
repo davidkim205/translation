6c7b942d1867bd9ce6553178d0ab48f61c50b7ad
sudog1 <eric2digit@gmail.com>
2024-04-01 13:52:44 +0900

    결과 파일 이름 통합

--- translation_self.py
+++ translation_self.py
@@ -46,23 +46,25 @@ def main():
         help="model path",
     )
     parser.add_argument("--output", default="", type=str, help="model path")
-    parser.add_argument("--model", default="iris_7b", type=str, help="model")
+    parser.add_argument(
+        "--model", default="davidkim205/iris-7b", type=str, help="model"
+    )
     args = parser.parse_args()
     json_data = load_json(args.input_file)
 
-    if args.model == "gugugo":
+    if args.model == "squarelike/Gugugo-koen-7B-V1.1":
         from models.gugugo import translate_en2ko, translate_ko2en
-    elif args.model == "madlad400":
+    elif args.model == "jbochi/madlad400-10b-mt":
         from models.madlad400 import translate_ko2en, translate_en2ko
-    elif args.model == "mbart50":
+    elif args.model == "facebook/mbart-large-50-many-to-many-mmt":
         from models.mbart50 import translate_en2ko, translate_ko2en
-    elif args.model == "nllb200":
+    elif args.model == "facebook/nllb-200-distilled-1.3B":
         from models.nllb200 import translate_ko2en, translate_en2ko
-    elif args.model == "TowerInstruct":
+    elif args.model == "Unbabel/TowerInstruct-7B-v0.1":
         from models.TowerInstruct import translate_ko2en, translate_en2ko
-    elif args.model == "synatra":
+    elif args.model == "maywell/Synatra-7B-v0.3-Translation":
         from models.synatra import translate_ko2en, translate_en2ko
-    elif args.model == "iris_7b":
+    elif args.model == "davidkim205/iris-7b":
         from models.iris_7b import translate_ko2en, translate_en2ko
         if args.model_path:
             from models.iris_7b import load_model
@@ -76,12 +78,14 @@ def main():
         def clean_text(text):
             if chat[0]["value"].find("한글로 번역하세요.") != -1:
                 cur_lang = "en"
-            
+
             else:
                 cur_lang = "ko"
             text = text.split("번역하세요.\n", 1)[-1]
             return text, cur_lang
+
         input, cur_lang = clean_text(input)
+
         def do_translation(text, cur_lang):
             trans = ""
             try:
@@ -90,8 +94,9 @@ def main():
                 else:
                     trans = translate_ko2en(text)
             except Exception as e:
-                    trans = ""
+                trans = ""
             return trans
+
         generation1 = do_translation(input, cur_lang)
         next_lang = "ko" if cur_lang == "en" else "en"
         generation2 = do_translation(generation1, next_lang)
@@ -107,17 +112,19 @@ def main():
             "lang": cur_lang,
             "model": args.model,
             "src": src,
-            'conversations':chat
+            "conversations": chat,
         }
         print(json.dumps(result, ensure_ascii=False, indent=2))
         if args.output:
             output = args.output
         else:
             if args.model_path:
-                filename = args.model_path.split('/')[-1]
+                filename = args.model.split("/")[-1]
+                model_num = args.model_path.split("/")[-1]
+                output = f"results_self/{filename}-{model_num}-result.jsonl"
             else:
-                filename = args.model
-            output = f"results_self/result_self-{args.model}-{filename}.jsonl"
+                filename = args.model.split("/")[-1]
+                output = f"results_self/{filename}-result.jsonl"
         save_json([result], output)
 
 
