2975848374aec729b8a636ab100b2d0966995506
sudog1 <eric2digit@gmail.com>
2024-04-03 21:51:07 +0900

    main 수정

--- create_table.py
+++ create_table.py
@@ -21,13 +21,14 @@ def aggregate_bleu(json_data, bleu_table, src_table, length_table):
             length_exceeds_count[data["model"]] += 1
     for model, row in src_table.items():
         src_table[model] = dict((attr, get_average(val)) for attr, val in row.items())
-        
+
     # bleu, duplicate, length exceeds 추가
     for model in src_table:
         bleu_table[model]["Average"].append(src_table[model]["Average"])
         bleu_table[model]["Bleu"] = src_table[model]["Average"]
         bleu_table[model]["Duplicate"] = duplicate_count[model]
         bleu_table[model]["Length Exceeds"] = length_exceeds_count[model]
+
 
 # results_self/에서 집계
 def aggregate_self(json_data, bleu_table, src_table, length_table):
@@ -39,6 +40,7 @@ def aggregate_self(json_data, bleu_table, src_table, length_table):
     for model in sbleu_score:
         bleu_table[model]["SBleu"] = get_average(sbleu_score[model])
         bleu_table[model]["Average"].append(bleu_table[model]["SBleu"])
+
 
 # results_length/에서 집계
 def aggregate_length(json_data, bleu_table, src_table, length_table):
@@ -73,7 +75,6 @@ def create():
         "bleu_by_length": length_table,
     }
 
-
     # bleu score 집계
     for dir, aggregate in results_dirs.items():
         json_data = []
@@ -83,7 +84,6 @@ def create():
             file_path = os.path.join(dir, filename)
             json_data += load_json(file_path)
         aggregate(json_data, *tables.values())
-
 
     # table dataframe 생성
     for table in tables:
@@ -96,22 +96,29 @@ def create():
         df.insert(
             0,
             "Type",
-            list(map(lambda x: "Cloud" if x in cloud_model else "HuggingFace", df["Model"]))
+            list(
+                map(
+                    lambda x: "Cloud" if x in cloud_model else "HuggingFace",
+                    df["Model"],
+                )
+            ),
         )
         df.insert(
             0, "Rank", df["Average"].rank(method="min", ascending=False).astype(int)
         )
         df = df.sort_values(by="Rank")
         tables[table] = df
-    
 
     return tables.values()
 
 
 def main():
-    tables = create()
-    for table in tables:
-        print(table.to_markdown(index=False), end="\n")
+    tables = list(create())
+    print("# dataframe")
+    print(tables[0], "\n\n")
+    print("# markdown")
+    print(tables[0].to_markdown(index=False))
+
 
 if __name__ == "__main__":
     main()
